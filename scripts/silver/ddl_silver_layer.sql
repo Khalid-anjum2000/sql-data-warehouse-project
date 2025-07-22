d9a789c775f33606fc6f76798447e364870c35da
khalid anjum <124677902+Khalid-anjum2000@users.noreply.github.com>
2025-07-22 22:37:28 +0530

    Update ddl_silver_layer.sql

--- scripts/silver/ddl_silver_layer.sql
+++ scripts/silver/ddl_silver_layer.sql
@@ -8,67 +8,67 @@ Script Purpose:
 ===============================================================================
 */
 
-drop table if exists silver.crm_cust_info;
-create table silver.crm_cust_info(
-	cst_id int,
-	cst_key varchar(50),
-	cst_firstname varchar(50),
-	cst_lastname varchar(50),
-	cst_material_status varchar(50),
-	cst_gndr varchar(50),
-	cst_create_date date,
-	dwh_create_date timestamp default current_timestamp
+DROP TABLE IF EXISTS silver.crm_cust_info;
+CREATE TABLE silver.crm_cust_info(
+	cst_id INT,
+	cst_key VARCHAR(50),
+	cst_firstname VARCHAR(50),
+	cst_lastname VARCHAR(50),
+	cst_material_status VARCHAR(50),
+	cst_gndr VARCHAR(50),
+	cst_create_date DATE,
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-drop table if exists silver.crm_prd_info;
-Create Table silver.crm_prd_info(
-	prd_id int,
-	cat_id varchar(50),
-	prd_key varchar(50),
-	prd_name varchar(50),
-	prd_cost int,
-	prd_line varchar(50),
-	prd_start_dt date,
-	prd_end_dt date
-	dwh_create_date timestamp default current_timestamp
+DROP TABLE IF EXISTS silver.crm_prd_info;
+CREATE TABLE silver.crm_prd_info(
+	prd_id INT,
+	cat_id VARCHAR(50),
+	prd_key VARCHAR(50),
+	prd_name VARCHAR(50),
+	prd_cost INT,
+	prd_line VARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE,
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-drop table if exists silver.crm_sales_details;
-Create Table silver.crm_sales_details(
-	sls_ord_num varchar(50),
-	sls_prd_key varchar(50),
-	sls_cust_id int,
-	sls_order_dt date,
-	sls_ship_dt date,
-	sls_due_dt date,
-	sls_sales int,
-	sls_quantity int,
-	sls_price int,
-	dwh_create_date timestamp default current_timestamp
+DROP TABLE IF EXISTS silver.crm_sales_details;
+CREATE TABLE silver.crm_sales_details(
+	sls_ord_num VARCHAR(50),
+	sls_prd_key VARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt DATE,
+	sls_ship_dt DATE,
+	sls_due_dt DATE,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT,
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-drop table if exists silver.erp_loc_a101;
-Create Table silver.erp_loc_a101(
-	cid varchar(50),
-	cntry varchar(50),
-	dwh_create_date timestamp default current_timestamp
-);
-
-
-drop table if exists silver.erp_cust_az12;
-Create Table silver.erp_cust_az12(
-	cid varchar(50),
-	bdate date,
-	gen varchar(50),
-	dwh_create_date timestamp default current_timestamp
+DROP TABLE IF EXISTS silver.erp_loc_a101;
+CREATE TABLE silver.erp_loc_a101(
+	cid VARCHAR(50),
+	cntry VARCHAR(50),
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 
-drop table if exists silver.erp_px_cat_g1v2;
-Create Table silver.erp_px_cat_g1v2(
-	id varchar(50),
-	cat varchar(50),
-	subcat varchar(50),
-	maintenance varchar(50)
-	dwh_create_date timestamp default current_timestamp
+DROP TABLE IF EXISTS silver.erp_cust_az12;
+CREATE TABLE silver.erp_cust_az12(
+	cid VARCHAR(50),
+	bdate DATE,
+	gen VARCHAR(50),
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+DROP TABLE IF EXISTS silver.erp_px_cat_g1v2;
+CREATE TABLE silver.erp_px_cat_g1v2(
+	id VARCHAR(50),
+	cat VARCHAR(50),
+	subcat VARCHAR(50),
+	maintenance VARCHAR(50),
+	dwh_create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
