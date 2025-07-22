c5605fe5c63a1a1d72005bd2692b75619dc53782
khalid anjum <124677902+Khalid-anjum2000@users.noreply.github.com>
2025-07-22 22:53:46 +0530

    Update ddl_bronze_layer.sql

--- scripts/bronze/ddl_bronze_layer.sql
+++ scripts/bronze/ddl_bronze_layer.sql
@@ -10,53 +10,56 @@ Script Purpose:
 */
 
 
-drop table if exists bronze.crm_cust_info;
-create table bronze.crm_cust_info(
-	cst_id int,
-	cst_key varchar(50),
-	cst_firstname varchar(50),
-	cst_lastname varchar(50),
-	cst_material_status varchar(50),
-	cst_gndr varchar(50),
-	cst_create_date date
+DROP TABLE IF EXISTS bronze.crm_cust_info;
+CREATE TABLE bronze.crm_cust_info(
+	cst_id INT,
+	cst_key VARCHAR(50),
+	cst_firstname VARCHAR(50),
+	cst_lastname VARCHAR(50),
+	cst_marital_status VARCHAR(50),
+	cst_gndr VARCHAR(50),
+	cst_create_date DATE
 );
 
-drop table if exists bronze.crm_prd_info;
-Create Table bronze.crm_prd_info(
-	prd_id int,
-	prd_key varchar(50),
-	prd_name varchar(50),
-	prd_cost int,
-	prd_line varchar(50),
-	prd_start_dt date,
-	prd_end_dt date
+DROP TABLE IF EXISTS bronze.crm_prd_info;
+CREATE TABLE bronze.crm_prd_info(
+	prd_id INT,
+	prd_key VARCHAR(50),
+	prd_name VARCHAR(50),
+	prd_cost INT,
+	prd_line VARCHAR(50),
+	prd_start_dt DATE,
+	prd_end_dt DATE
 );
 
-drop table if exists bronze.crm_sales_details;
-Create Table bronze.crm_sales_details(
-	sls_ord_num varchar(50),
-	sls_prd_key varchar(50),
-	sls_cust_id int,
-	sls_order_dt int,
-	sls_ship_dt int,
-	sls_due_dt int,
-	sls_sales int,
-	sls_quantity int,
-	sls_price int 
+DROP TABLE IF EXISTS bronze.crm_sales_details;
+CREATE TABLE bronze.crm_sales_details(
+	sls_ord_num VARCHAR(50),
+	sls_prd_key VARCHAR(50),
+	sls_cust_id INT,
+	sls_order_dt INT,
+	sls_ship_dt INT,
+	sls_due_dt INT,
+	sls_sales INT,
+	sls_quantity INT,
+	sls_price INT
 );
 
-drop table if exists bronze.erp_cust_az12;
-Create Table bronze.erp_cust_az12(
-	cid varchar(50),
-	bdate date,
-	gen varchar(50)
+DROP TABLE IF EXISTS bronze.erp_loc_a101;
+CREATE TABLE bronze.erp_loc_a101(
+	cid VARCHAR(50),
+	cntry VARCHAR(50)
 );
 
-drop table if exists bronze.erp_loc_a101;
-Create Table bronze.erp_loc_a101(
-	cid varchar(50),
-	cntry varchar(50)
+DROP TABLE IF EXISTS bronze.erp_cust_az12;
+CREATE TABLE bronze.erp_cust_az12(
+	cid VARCHAR(50),
+	bdate DATE,
+	gen VARCHAR(50)
 );
+
+
+
 
 drop table if exists bronze.erp_px_cat_g1v2;
 Create Table bronze.erp_px_cat_g1v2(
@@ -65,4 +68,21 @@ cat varchar(50),
 subcat varchar(50),
 maintenance varchar(50)
 );
+
+DROP TABLE IF EXISTS bronze.erp_px_cat_g1v2;
+CREATE TABLE bronze.erp_px_cat_g1v2(
+	id VARCHAR(50),
+	cat VARCHAR(50),
+	subcat VARCHAR(50),
+	maintenance VARCHAR(50)
+);
+
+
+
+=========
+
+
+
+
+
 
