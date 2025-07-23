fd0acf7d76904bd4e75de45f691093d45a24e6c7
khalid anjum <124677902+Khalid-anjum2000@users.noreply.github.com>
2025-07-23 23:57:57 +0530

    Update view_customer_gold.sql

--- scripts/gold/view_customer_gold.sql
+++ scripts/gold/view_customer_gold.sql
@@ -46,4 +46,26 @@ on pr.cat_id=pc.id
 where pr.prd_end_dt is null
 
 
+--fact sales
+CREATE VIEW gold.fact_sales as
+
+SELECT 
+sls_ord_num as order_number,
+pr.product_key,
+cu.customer_key,
+sls_order_dt as order_date,
+sls_ship_dt as shipping_date,
+sls_due_dt as due_date,
+sls_sales as sales_amount,
+sls_quantity as quantity,
+sls_price as price 
+FROM silver.crm_sales_details sd
+left join gold.dim_products pr
+on sd.sls_prd_key = pr.product_number
+left join gold.dim_customers as cu
+on sd.sls_cust_id = cu.customer_id
+
+
+
+
 
