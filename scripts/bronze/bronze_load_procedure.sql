a03d9efa63d3350a2c890d9ebb4ea84f6e77cf82
khalid anjum <124677902+Khalid-anjum2000@users.noreply.github.com>
2025-07-24 22:58:47 +0530

    Update bronze_load_procedure.sql

--- scripts/bronze/bronze_load_procedure.sql
+++ scripts/bronze/bronze_load_procedure.sql
@@ -1,8 +1,22 @@
--- ============================================================================
--- Procedure: bronze.load_bronze
--- Description: Loads data into the 'bronze' schema from external CSV files.
---              Truncates existing tables and imports fresh data using COPY.
--- ============================================================================
+/*
+===============================================================================
+Stored Procedure: Load Bronze Layer (Source → Bronze) - PostgreSQL
+===============================================================================
+Script Purpose:
+    This stored procedure loads data into the 'bronze' schema from external CSV files.
+    It performs the following actions:
+    - Truncates the bronze tables before loading data.
+    - Uses the `COPY` command to load data from CSV files into bronze tables.
+
+Parameters:
+    None.
+    This stored procedure does not accept any parameters or return any values.
+
+Usage Example:
+    CALL bronze.load_bronze();
+===============================================================================
+*/
+
 
 CREATE OR REPLACE PROCEDURE bronze.load_bronze()
 LANGUAGE plpgsql
