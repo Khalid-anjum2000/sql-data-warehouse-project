8d9e59147af667081e9cd1b01d0507d55a25ede9
khalid anjum <124677902+Khalid-anjum2000@users.noreply.github.com>
2025-07-24 23:06:34 +0530

    Update view_customer_gold.sql

--- scripts/gold/view_customer_gold.sql
+++ scripts/gold/view_customer_gold.sql
@@ -1,7 +1,24 @@
+/*
+===============================================================================
+DDL Script: Create Gold Views
+===============================================================================
+Script Purpose:
+    This script creates views for the Gold layer in the data warehouse. 
+    The Gold layer represents the final dimension and fact tables (Star Schema).
 
+    Each view performs transformations and combines data from the Silver layer 
+    to produce a clean, enriched, and business-ready dataset.
 
+Usage:
+    - These views can be queried directly for analytics and reporting.
+    - Run this script after the Silver layer is fully populated.
+===============================================================================
+*/
 
---Customer dimesion
+-- =============================================================================
+-- Create Dimension: gold.dim_customers
+-- =============================================================================
+
 CREATE VIEW gold.dim_customers as
 select
 	row_number() over( order by ci.cst_id) as customer_key,
